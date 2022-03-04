e4b6480abbb1ea97a15c3caf02e4084af93ec9c3
Dylan <dmarco96@gmail.com>
2022-03-03 21:54:12 -0500

    ci(expense-tracker-web): add deploy scripts

--- tools/deployers/expense-tracker-web.deploy.sh
+++ tools/deployers/expense-tracker-web.deploy.sh
@@ -4,6 +4,6 @@ echo "Deploying setscore-web"
 echo "======================"
 echo ""
 
-export VERCEL_PROJECT_ID=$SET_SCORE_VERCEL_PROJECT_ID
+export VERCEL_PROJECT_ID=$EXPENSE_TRACKER_VERCEL_PROJECT_ID
 
 vercel ./ -t $VERCEL_ACCESS_TOKEN --prod --force
