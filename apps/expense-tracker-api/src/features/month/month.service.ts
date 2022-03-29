a7c99d1d6378952e09f46371f81f9f9d98ccccd1
Dylan <dmarco96@gmail.com>
2022-03-29 18:56:01 -0400

    fix(expense-tracker-api): fix sorting expenses no sort provided

--- apps/expense-tracker-api/src/features/month/month.service.ts
+++ apps/expense-tracker-api/src/features/month/month.service.ts
@@ -150,79 +150,7 @@ export class MonthService {
                 }))
         }));
 
-        return expenseDtos.sort((a, b) => {
-            if (
-                paginationAndSort.sortColumn === 'day' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return a.day - b.day;
-            } else if (
-                paginationAndSort.sortColumn === 'day' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return b.day - a.day;
-            } else if (
-                paginationAndSort.sortColumn === 'amountCents' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return b.amountCents - a.amountCents;
-            } else if (
-                paginationAndSort.sortColumn === 'amountCents' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return a.amountCents - b.amountCents;
-            } else if (
-                paginationAndSort.sortColumn === 'split' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return b.split - a.split;
-            } else if (
-                paginationAndSort.sortColumn === 'split' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return a.split - b.split;
-            } else if (
-                paginationAndSort.sortColumn === 'name' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return a.name.localeCompare(b.name);
-            } else if (
-                paginationAndSort.sortColumn === 'name' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return b.name.localeCompare(a.name);
-            } else if (
-                paginationAndSort.sortColumn === 'paymentMethod' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return (
-                    a.paymentMethod?.name.localeCompare(
-                        b.paymentMethod?.name
-                    ) ?? 0
-                );
-            } else if (
-                paginationAndSort.sortColumn === 'paymentMethod' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return (
-                    b.paymentMethod?.name.localeCompare(
-                        a.paymentMethod?.name
-                    ) ?? 0
-                );
-            } else if (
-                paginationAndSort.sortColumn === 'category' &&
-                paginationAndSort.sortDirection === SortDirection.Asc
-            ) {
-                return a.category?.name.localeCompare(b.category?.name) ?? 0;
-            } else if (
-                paginationAndSort.sortColumn === 'category' &&
-                paginationAndSort.sortDirection === SortDirection.Desc
-            ) {
-                return b.category?.name.localeCompare(a.category?.name) ?? 0;
-            } else {
-                return a.day - b.day;
-            }
-        });
+        return this._sortExpenses(expenseDtos, paginationAndSort);
     }
 
     public async createOrUpdateExpense(
@@ -371,5 +299,92 @@ export class MonthService {
         }
 
         await this._expenseRepository.delete(expenseId);
+    }
+
+    private _sortExpenses(
+        expenses: ExpenseDto[],
+        paginationAndSort: PaginationAndSort
+    ): ExpenseDto[] {
+        return expenses.sort((a, b) => {
+            if (
+                !paginationAndSort ||
+                !paginationAndSort.sortColumn ||
+                !paginationAndSort.sortDirection
+            ) {
+                return a.day - b.day;
+            }
+
+            if (
+                paginationAndSort.sortColumn === 'day' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return a.day - b.day;
+            } else if (
+                paginationAndSort.sortColumn === 'day' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return b.day - a.day;
+            } else if (
+                paginationAndSort.sortColumn === 'amountCents' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return b.amountCents - a.amountCents;
+            } else if (
+                paginationAndSort.sortColumn === 'amountCents' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return a.amountCents - b.amountCents;
+            } else if (
+                paginationAndSort.sortColumn === 'split' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return b.split - a.split;
+            } else if (
+                paginationAndSort.sortColumn === 'split' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return a.split - b.split;
+            } else if (
+                paginationAndSort.sortColumn === 'name' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return a.name.localeCompare(b.name);
+            } else if (
+                paginationAndSort.sortColumn === 'name' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return b.name.localeCompare(a.name);
+            } else if (
+                paginationAndSort.sortColumn === 'paymentMethod' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return (
+                    a.paymentMethod?.name.localeCompare(
+                        b.paymentMethod?.name
+                    ) ?? 0
+                );
+            } else if (
+                paginationAndSort.sortColumn === 'paymentMethod' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return (
+                    b.paymentMethod?.name.localeCompare(
+                        a.paymentMethod?.name
+                    ) ?? 0
+                );
+            } else if (
+                paginationAndSort.sortColumn === 'category' &&
+                paginationAndSort.sortDirection === SortDirection.Asc
+            ) {
+                return a.category?.name.localeCompare(b.category?.name) ?? 0;
+            } else if (
+                paginationAndSort.sortColumn === 'category' &&
+                paginationAndSort.sortDirection === SortDirection.Desc
+            ) {
+                return b.category?.name.localeCompare(a.category?.name) ?? 0;
+            } else {
+                return a.day - b.day;
+            }
+        });
     }
 }
